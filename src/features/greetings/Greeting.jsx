1fae1ec563da023ab58af6470fb525ba04afc18f
rbhatt199924 <rbhatt199924@gmail.com>
2023-05-02 16:40:11 +0530

    add greeting component & store

---
+++ src/features/greetings/Greeting.jsx
@@ -0,0 +1,26 @@
+import {React, useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import style from './greeting.module.css'
+import { greetingsRequest } from './greetingReducer.js'
+
+
+export default function Greeting() {
+  const dispatch = useDispatch();
+  const { greeting, loading, error} = useSelector(state => state.greeting)
+  useEffect(() => {
+    dispatch(greetingsRequest())
+  }, [dispatch])
+
+  const handler = () => {
+    dispatch(greetingsRequest())
+  }
+  
+  return (
+    <div className={style.container}>
+      {loading && <div className={style.loader}></div>}
+      {error && <p>Error!</p>}
+      <h1>{!loading && greeting}</h1>
+      {!loading && <button className={style.button1} onClick={handler}>Refresh</button>}
+    </div>
+  )
+}
